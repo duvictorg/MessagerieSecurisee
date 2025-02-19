f5e232b3daad9331c38933fed307f1d367e7fcdf
Darkside74 <158472602+Darkside74@users.noreply.github.com>
2025-02-19 10:14:05 +0100

    Update chiffrement.py
    modif avec cesar

--- chiffrement.py
+++ chiffrement.py
@@ -1,20 +1,14 @@
-text = input("Tapez le message: ")
-cle = "Bonjour"
-
 def décalage(char, shift, encrypt=True):
     return chr((ord(char) + shift) % 128) if encrypt else chr((ord(char) - shift) % 128)
 
 def vigenere(text, cle, encrypt=True):
     return ''.join(décalage(text[i], ord(cle[i % len(cle)]), encrypt) for i in range(len(text)))
 
-# Chiffrement
-text = vigenere(text, cle)
-print("Texte chiffré:", text)
-
-# Déchiffrement
-text_dechiffre = vigenere(text, cle, encrypt=False)
-print("Texte déchiffré:", text_dechiffre)
-
-
-
-# --------------- challenge 1 ---------------------
+def cesar(text, shift, encrypt=True):
+    if encrypt:
+        for abcd in ["abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"]:
+            text = "".join([abcd[(abcd.index(char) + shift) % 26] if char in abcd else char for char in text])
+    else:
+        for abcd in ["abcdefghijklmnopqrstuvwxyz", "ABCDEFGHIJKLMNOPQRSTUVWXYZ"]:
+            text = "".join([abcd[(abcd.index(char) - shift) % 26] if char in abcd else char for char in text])
+    return text
