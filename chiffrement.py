51fb6d490ae2fa422db82a464425c231799e5598
Darkside74 <158472602+Darkside74@users.noreply.github.com>
2025-02-19 09:46:55 +0100

    Update chiffrement.py
    modif vignenere

--- chiffrement.py
+++ chiffrement.py
@@ -1,45 +1,20 @@
 text = input("Tapez le message: ")
-clé = "Bonjour"
-
-def taille_clé(clé, length):
-    clé_répété = (clé * (length // len(clé))) + clé[:length % len(clé)]
-    return clé_répété
+cle = "Bonjour"
 
 def décalage(char, shift, encrypt=True):
-    if char.isupper():
-        base = 65
-    elif char.islower():
-        base = 97
-    else:
-        return char
+    return chr((ord(char) + shift) % 128) if encrypt else chr((ord(char) - shift) % 128)
 
-    if encrypt:
-        return chr((ord(char) - base + shift) % 26 + base)
-    else:
-        return chr((ord(char) - base - shift) % 26 + base)
-
-def vigenere(plain_text, key):
-    """Chiffre le texte clair en utilisant la clé de Vigenère."""
-    key = key.upper()
-    plain_text = plain_text.upper()
-    repeated_key = repeat_key(key, len(plain_text))
-    encrypted_text = ''.join(shift_char(plain_text[i], ord(repeated_key[i]) - 65) for i in range(len(plain_text)))
-    return encrypted_text
-
-def vigenere_decrypt(encrypted_text, key):
-    """Déchiffre le texte chiffré en utilisant la clé de Vigenère."""
-    key = key.upper()
-    encrypted_text = encrypted_text.upper()
-    repeated_key = repeat_key(key, len(encrypted_text))
-    decrypted_text = ''.join(shift_char(encrypted_text[i], ord(repeated_key[i]) - 65, encrypt=False) for i in range(len(encrypted_text)))
-    return decrypted_text
-
-
+def vigenere(text, cle, encrypt=True):
+    return ''.join(décalage(text[i], ord(cle[i % len(cle)]), encrypt) for i in range(len(text)))
 
 # Chiffrement
-encrypted_text = vigenere_encrypt(plain_text, key)
-print("Texte chiffré:", encrypted_text)
+text = vigenere(text, cle)
+print("Texte chiffré:", text)
 
 # Déchiffrement
-decrypted_text = vigenere_decrypt(encrypted_text, key)
-print("Texte déchiffré:", decrypted_text)
+text_dechiffre = vigenere(text, cle, encrypt=False)
+print("Texte déchiffré:", text_dechiffre)
+
+
+
+# --------------- challenge 1 ---------------------
